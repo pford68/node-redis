1d793aebed9ccbdb5208462152375ae9d10cee45
pford68 <pford68@earthlink.net>
2019-03-01 16:59:22 -0500

    Requiring Conneciton in app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 
+require('./dao/Connection');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
